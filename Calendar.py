b9fe11123a5daabea0ad7d7c0b6da6197193ed29
Meshack Kipkorir <meshackkipkorir105@gmail.com>
2019-11-19 08:32:40 -0500

    implemented welcome function

--- Calendar.py
+++ Calendar.py
@@ -3,3 +3,15 @@ Simple Command Line calendar program
 """
 
 from time import sleep,strftime
+
+#welcome user function
+
+name = "" #your name goes here
+def welcome():
+  print "Welcome " + name
+  print "*** Calendar is opening"
+  sleep(1)
+  print "Today is : " + strftime("%A %B %d, %Y")
+  print "The time is :" + strftime("%H : %M : %S")
+  sleep(1) 
+  print "What would you like to do ?"
