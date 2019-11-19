cff66fc4c4349cf13154ef7215d7c201eb58e3b0
Meshack Kipkorir <meshackkipkorir105@gmail.com>
2019-11-19 08:34:10 -0500

    started the calendar function
    prompt user for various inputs(U,V,D,X,A)

--- Calendar.py
+++ Calendar.py
@@ -15,3 +15,9 @@ def welcome():
   print "The time is :" + strftime("%H : %M : %S")
   sleep(1) 
   print "What would you like to do ?"
+  
+def start_calendar():
+  welcome()
+  start = True
+  while start:
+    user_choice = raw_input("Enter A to Add, U to update,V to view ,D to delete , X to exit :").upper()
